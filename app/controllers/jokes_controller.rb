d19695a6517eaaa0518fba4bea2deeec31985288
Greg Seguin <greg.seguin@gmail.com>
2015-10-26 02:02:55 +0000

    finished

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -1,11 +1,23 @@
 class JokesController < ApplicationController
 
 	def index
-		@joke = Joke.first
+		@joke = Joke.order("RANDOM()").first
 	end
 
 	def new
 		@joke = Joke.new
 	end
+
+	def create
+		Joke.create(joke_params)
+		redirect_to root_path
+	end
+
+ 	private
+
+
+  	def joke_params
+    	params.require(:joke).permit(:saying, :author, :title)
+  	end
 
 end
