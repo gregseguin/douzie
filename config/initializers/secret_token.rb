490b253e62c981561b061607d48f0f7a97e793a5
Greg Seguin <greg.seguin@gmail.com>
2015-10-26 00:22:09 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Douzie::Application.config.secret_key_base = '9de923f45df3745652fad6434c8ae4a6e4167b10df43a616d11d8c386e0460de4de2d0631ce19906bb37a892c5564a6565c499c71488a979a9738f13ae7de3ae'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Douzie::Application.config.session_store :cookie_store, key: '_douzie_session'
