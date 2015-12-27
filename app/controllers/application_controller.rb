c36de79d87ef539cc3493ce31dbaa946199a4b80
Greg Seguin <greg.seguin@gmail.com>
2015-12-27 01:33:10 +0000

    Added Nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :get_navigation
+
+private
+	def get_navigation
+		@navitems = Tag.all.order('name ASC')
+	end
 
 end
--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,5 +1,6 @@
 class SitemapController < ApplicationController
   respond_to :xml
+  
   def index
     @tags = Tag.order("created_at DESC")
     @jokes = Joke.order("created_at DESC")
