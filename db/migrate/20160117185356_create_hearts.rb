fabc5d46777a4e5b021a952259d59f4c5fc575f8
Greg Seguin <greg.seguin@gmail.com>
2016-01-17 22:04:46 +0000

    tweaks

---
+++ db/migrate/20160117185356_create_hearts.rb
@@ -0,0 +1,9 @@
+class CreateHearts < ActiveRecord::Migration
+  def change
+    create_table :hearts do |t|
+      t.belongs_to :joke, index: true
+      t.belongs_to :user, index: true
+      t.timestamps null: false
+    end
+  end
+end
